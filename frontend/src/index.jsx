928a2873f53adff2995b965905385c527e575c31
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-16 12:32:45 +0900

    #5 make user register screen

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import ReactDom from "react-dom"
 import {ShowRankSample} from "./ShowRankSample"
+import {UserRegister} from "./UserRegister"
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <ShowRankSample />
+        </div>
+        <div>
+          <UserRegister />
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDom.render(
-  <ShowRankSample />,
+  <Index />,
   document.getElementById("root")
 )

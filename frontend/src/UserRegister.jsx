705a1e513cefab09a317ffa7da35dd5566b82e2c
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-16 13:33:33 +0900

    ボタンを押したら、コンソールにformのvalueを出力

--- frontend/src/UserRegister.jsx
+++ frontend/src/UserRegister.jsx
@@ -1,13 +1,33 @@
 import React from "react"
+import ReactDOM from "react-dom"
 import { render } from 'react-dom'
 
 class UserRegisterForm extends React.Component {
+  handleSubmit(event) {
+    // ボタンを押すことによる遷移を抑制
+    event.preventDefault();
+    var name = event.target.name.value;
+    var email = event.target.email.value;
+    var password = event.target.password.value;
+    console.log(name, email, password);
+    if (!name || !email || !password) {
+      return;
+    }
+
+    postForm({name: name, email: email, password: password});
+    // valueを空にする
+    ReactDOM.findDOMNode(event.target.name).value = '';
+    ReactDOM.findDOMNode(event.target.email).value = '';
+    ReactDOM.findDOMNode(event.target.password).value = '';
+    return;
+  }
+
   render() {
     return (
-      <form className="commentForm">
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="email" placeholder="email" />
+        <input type="text" name="password" placeholder="password" />
         <input type="submit" value="Post" />
       </form>
     );
@@ -20,4 +40,8 @@ export class UserRegister extends React.Component {
       <UserRegisterForm />
     )
   }
+}
+
+function postForm(form) {
+  console.log(form);
 }

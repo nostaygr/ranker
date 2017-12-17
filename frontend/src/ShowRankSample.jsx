dd03ea6be59ba4a708c0f52f7f9f6a6fd5ae1837
longtime1116 <longtime1116@gmail.com>
2017-12-17 22:11:51 +0900

    add eslint-config-airbnb

--- frontend/src/ShowRankSample.jsx
+++ frontend/src/ShowRankSample.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
-const REQEST_URL = "http://localhost:3000/subjects/index";
+const REQEST_URL = 'http://localhost:3000/subjects/index';
 
 export class ShowRankSample extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
     };
   }
 
@@ -18,20 +18,14 @@ export class ShowRankSample extends React.Component {
   fetchData() {
     fetch(REQEST_URL)
       .then(response => response.json())
-      .then(responseData => {
+      .then((responseData) => {
         this.setState({
-          data: responseData
+          data: responseData,
         });
       });
   }
 
   render() {
-    return (
-      <ul>
-        {this.state.data.map(subject => {
-          return <li key={subject.id}>{subject.title}</li>;
-        })}
-      </ul>
-    );
+    return <ul>{this.state.data.map(subject => <li key={subject.id}>{subject.title}</li>)}</ul>;
   }
 }
--- frontend/src/UserRegister.jsx
+++ frontend/src/UserRegister.jsx
@@ -1,40 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { render } from "react-dom";
-import "whatwg-fetch";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import 'whatwg-fetch';
 
 class UserRegisterForm extends React.Component {
   handleSubmit(event) {
     // ボタンを押すことによる遷移を抑制
     event.preventDefault();
-    var name = event.target.name.value;
-    var email = event.target.email.value;
-    var password = event.target.password.value;
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
     console.log(name, email, password);
     if (!name || !email || !password) {
       return;
     }
 
-    var form = new FormData();
-    form.append("user[name]", name);
-    form.append("user[email]", email);
-    form.append("user[password]", password);
+    const form = new FormData();
+    form.append('user[name]', name);
+    form.append('user[email]', email);
+    form.append('user[password]', password);
     postForm(form);
 
     // valueを空にする
-    ReactDOM.findDOMNode(event.target.name).value = "";
-    ReactDOM.findDOMNode(event.target.email).value = "";
-    ReactDOM.findDOMNode(event.target.password).value = "";
-    return;
+    ReactDOM.findDOMNode(event.target.name).value = '';
+    ReactDOM.findDOMNode(event.target.email).value = '';
+    ReactDOM.findDOMNode(event.target.password).value = '';
   }
 
   render() {
     return (
-      <form
-        id="user-register"
-        className="commentForm"
-        onSubmit={this.handleSubmit}
-      >
+      <form id="user-register" className="commentForm" onSubmit={this.handleSubmit}>
         <input type="text" name="name" placeholder="name" />
         <input type="text" name="email" placeholder="email" />
         <input type="text" name="password" placeholder="password" />
@@ -52,12 +47,12 @@ export class UserRegister extends React.Component {
 
 function postForm(form) {
   console.log(form);
-  fetch("http://localhost:3000/users", {
+  fetch('http://localhost:3000/users', {
     header: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
     },
-    method: "POST",
-    body: form
+    method: 'POST',
+    body: form,
   });
 }

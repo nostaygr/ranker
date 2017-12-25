e574c39eecc250ede9c303017d643db149f2e6dc
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-25 19:48:39 +0900

    add material-ui appbar to header

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import FlatButton from 'material-ui/FlatButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
 import { render } from 'react-dom';
 import { Link } from 'react-router-dom';
 
@@ -13,19 +17,32 @@ const links = [
   },
 ];
 
+const buttonStyle = {
+  color: 'white',
+  marginTop: 5
+}
+
 export class Header extends React.Component {
   render() {
     const linkComponent = links.map(({ path, name }, key) => (
-      <li>
-        <Link to={path}>{name}</Link>
-      </li>
+      <FlatButton label={name} key={key} style={buttonStyle} containerElement={<Link to={path} />} />
     ));
 
     return (
       <header>
-        <nav>
-          <ul>{linkComponent}</ul>
-        </nav>
+        <AppBar
+          title={<span>Ranker</span>}
+          iconElementLeft={
+            <IconButton containerElement={<Link to="/" />}>
+              <ActionHome />
+            </IconButton>
+          }
+          iconElementRight={
+            <div>
+              {linkComponent}
+            </div>
+          }
+        />
       </header>
     );
   }

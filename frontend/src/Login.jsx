465a0d39494e5fff069d2acba904ade755005c5b
longtime1116 <longtime1116@gmail.com>
2017-12-28 14:45:34 +0900

    issue_22 add login page

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import history from './history';
+import 'whatwg-fetch';
+
+class LoginForm extends React.Component {
+  handleSubmit(event) {
+    // ボタンを押すことによる遷移を抑制
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    if (!email || !password) {
+      return;
+    }
+
+    const form = new FormData();
+    form.append('email', email);
+    form.append('password', password);
+    postForm(form);
+
+    // valueを空にする
+    ReactDOM.findDOMNode(event.target.email).value = '';
+    ReactDOM.findDOMNode(event.target.password).value = '';
+  }
+
+  render() {
+    return (
+      <form id="logIn" className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" name="email" placeholder="email" />
+        <input type="text" name="password" placeholder="password" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+}
+
+export class Login extends React.Component {
+  render() {
+    return <LoginForm />;
+  }
+}
+
+function postForm(form) {
+  fetch('http://localhost:3000/v1/auth/sign_in', {
+    header: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: form,
+  })
+    .then((response) => {
+      if (response.status == 200) {
+        history.push('/');
+      } else {
+        throw Error(response.statusText);
+      }
+    })
+    .catch((error) => {
+      alert('failed to signin');
+    });
+}
--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import { Header } from './Header';
 import { ShowRankSample } from './ShowRankSample';
+import { Login } from './Login';
 import { UserRegister } from './UserRegister';
 
 export class App extends React.Component {
@@ -13,6 +14,7 @@ export class App extends React.Component {
         </div>
         <Switch>
           <Route exact path="/" component={ShowRankSample} />
+          <Route path="/login" component={Login} />
           <Route path="/signup" component={UserRegister} />
         </Switch>
       </div>

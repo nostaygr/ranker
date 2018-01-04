a87ff896e33fabd4e2272d24e805fce1a208916c
longtime1116 <longtime1116@gmail.com>
2018-01-05 08:45:13 +0900

    change Logout from a react component class to function

--- frontend/src/Logout.jsx
+++ frontend/src/Logout.jsx
@@ -1,19 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
 import history from './history';
-import 'whatwg-fetch';
 
-export class Logout extends React.Component {
-  componentDidMount() {
-    this.logout();
-  }
-
-  logout() {
-    localStorage.setItem('login', 'false');
-    history.push('/');
-  }
-
-  render() {
-    return null;
-  }
+export function Logout() {
+  localStorage.setItem('login', 'false');
+  history.push('/');
+  return null;
 }

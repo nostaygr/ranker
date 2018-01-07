ff0f86fb34cfebdb7f53c487bfa8675e71353394
longtime1116 <longtime1116@gmail.com>
2018-01-08 00:27:03 +0900

    Revert "twitter authentication(backend)"
    This reverts commit fd17663e8297040ba735d639461c184c0ac8d3a7.

--- app/controllers/twitter_authentication_controller.rb
+++
@@ -1,7 +0,0 @@
-class TwitterAuthenticationController < ApplicationController
-  def callback
-    omniauth = request.env['omniauth.auth']
-    omniauth.credentials.token
-    omniauth.credentials.secret
-  end
-end

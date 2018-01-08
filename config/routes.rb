ab62375a8167bc37c6fc037602ca9a17d765a743
longtime1116 <longtime1116@gmail.com>
2018-01-08 15:06:52 +0900

    update route(omniauth)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   namespace :v1 do
     mount_devise_token_auth_for 'User',
                                 at: 'auth',
-                                controllers: { registrations: 'v1/auth/registrations' }
+                                controllers: { registrations: 'v1/auth/registrations',
+                                               omniauth_callbacks: 'v1/auth/omniauth_callbacks'}
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

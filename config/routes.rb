d04919a1c1d7b9d1ca5c1ff32173e0837ca74673
longtime1116 <longtime1116@gmail.com>
2018-01-10 09:07:49 +0900

    update subjects routing nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'subjects/index'
-  post 'subjects', to: 'subjects#create'
-
   namespace :v1 do
     mount_devise_token_auth_for 'User',
                                 at: 'auth',
                                 controllers: { registrations: 'v1/auth/registrations' }
+  end
+  resources :users, shallow: true, only: [] do
+    resources :subjects, only: [:index, :show, :create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

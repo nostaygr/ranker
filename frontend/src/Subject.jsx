791ab9d95bed828a50667a8e2db7d252061523a1
Aiki YOUGAI <phoenix22.you@gmail.com>
2018-01-11 21:06:44 +0900

    make frontend/src/Subject.jsx

---
+++ frontend/src/Subject.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class Subject extends React.Component {
+  render() {
+    const id = this.props.match.params.id
+    return (
+      <div>{id}</div>
+    );
+  }
+}

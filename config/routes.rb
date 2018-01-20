0e5191de6fe33b8991a4f1979961d6839757e4fc
longtime1116 <longtime1116@gmail.com>
2018-01-20 17:54:08 +0900

    update routes.rb about items

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'items/show'
+
+  get 'items/index'
+
+  get 'items/destroy'
+
   namespace :v1 do
     mount_devise_token_auth_for 'User',
                                 at: 'auth',
                                 controllers: { registrations: 'v1/auth/registrations' }
   end
   resources :users, shallow: true, only: [] do
-    resources :subjects, only: [:index, :show, :create, :destroy]
+    resources :subjects, shallow: true, only: [:index, :show, :create, :destroy] do
+      resources :items, only: [:index, :show, :create, :destroy]
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

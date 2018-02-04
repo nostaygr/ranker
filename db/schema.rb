f6d12b2b9f171bb911b248da6370b1f82b411bc8
longtime1116 <longtime1116@gmail.com>
2018-02-04 16:35:56 +0900

    add nickname and image to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119140011) do
+ActiveRecord::Schema.define(version: 20180204073120) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "rank"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20180119140011) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true

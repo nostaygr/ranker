24f65bfe0cac44e79ebac7809f1a56e08cbedcc4
longtime1116 <longtime1116@gmail.com>
2018-02-08 09:09:13 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,12 @@ User.create(name: "foo", email: "foo@gmail.com", password: "foofoofoo", password
 Subject.create(user_id: 1, is_public: true, title: "好きな映画ベスト10")
 Subject.create(user_id: 1, is_public: false, title: "嫌いな野菜ベスト5")
 Subject.create(user_id: 2, is_public: false, title: "好きなたこ焼きの店ベスト3")
+Subject.create(user_id: 2, is_public: true, title: "好きな肉の店ベスト3")
 
 Item.create(subject_id: 1, rank: 1, name: "ダークナイト")
 Item.create(subject_id: 1, rank: 2, name: "マッドマックス怒りのデスロード")
 Item.create(subject_id: 2, rank: 1, name: "パクチー")
+Item.create(subject_id: 3, rank: 1, name: "金だこ")
+Item.create(subject_id: 3, rank: 2, name: "銀だこ")
+Item.create(subject_id: 4, rank: 1, name: "肉肉")
+Item.create(subject_id: 4, rank: 2, name: "肉肉肉")

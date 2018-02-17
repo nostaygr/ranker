9e5f3eaef383d0a886e952a7de95285c9050f29e
Aiki YOUGAI <phoenix22.you@gmail.com>
2018-02-18 01:06:07 +0900

    add validation in signup

--- frontend/src/Signup.jsx
+++ frontend/src/Signup.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
+import TextField from 'material-ui/TextField';
 import 'whatwg-fetch';
 import { signup } from './common';
 
 class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameErrorText: '',
+      emailErrorText: '',
+      passwordErrorText: '',
+    };
+  }
+
+  onTextFieldChange(event) {
+    if (event.target.value) {
+      eval("this.setState({" + event.target.name + "ErrorText: '', })");
+    }
+  }
+
+  validate(name, email, password) {
+    const field = ["name", "email", "password"];
+    let validationError = false
+    for (let f of field) {
+      if (!eval(f)) {
+        eval("this.setState({" + f + "ErrorText: f + ' is invalid', })");
+        validationError = true;
+      } else {
+        eval("this.setState({" + f + "ErrorText: '', })");
+      }
+    }
+
+    if (validationError) {
+      return true
+    }
+    return false
+  }
+
   handleSubmit(event, onClick) {
     // ボタンを押すことによる遷移を抑制
     event.preventDefault();
     const name = event.target.name.value;
     const email = event.target.email.value;
     const password = event.target.password.value;
-    if (!name || !email || !password) {
+    if(this.validate(name, email, password)) {
       return;
     }
     onClick(name, email, password);
@@ -29,9 +63,27 @@ class SignupForm extends React.Component {
         className="commentForm"
         onSubmit={event => this.handleSubmit(event, this.props.onClick)}
       >
-        <input type="text" name="name" placeholder="name" />
-        <input type="text" name="email" placeholder="email" />
-        <input type="text" name="password" placeholder="password" />
+        <TextField
+          name="name"
+          placeholder="name"
+          errorText={this.state.nameErrorText}
+          onChange={this.onTextFieldChange.bind(this)}
+        />
+        <br />
+        <TextField
+          name="email"
+          placeholder="email"
+          errorText={this.state.emailErrorText}
+          onChange={this.onTextFieldChange.bind(this)}
+        />
+        <br />
+        <TextField
+          name="password"
+          placeholder="password"
+          errorText={this.state.passwordErrorText}
+          onChange={this.onTextFieldChange.bind(this)}
+        />
+        <br />
         <input type="submit" value="Post" />
       </form>
     );
